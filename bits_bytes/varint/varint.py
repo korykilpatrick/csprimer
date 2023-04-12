7abe12486197edb08eb989e9de62ae4db06549ca
Kory Kilpatrick <koryrkilpatrick@gmail.com>
2023-04-11 21:06:27 -0500

    final decode func and some benchmarks on 2017 mbook pro

--- bits_bytes/varint/varint.py
+++ bits_bytes/varint/varint.py
@@ -1,6 +1,7 @@
 # Implementing protobuf varint encoding as detailed https://protobuf.dev/programming-guides/encoding/#varints
 import struct 
 import cProfile
+import time
 
 def encode(number):
     out = []
@@ -15,34 +16,11 @@ def encode(number):
         
     return bytes(out)
 
-"""real    25m25.770s
-user    25m14.747s
-sys     0m4.254s"""
-def decode1(varint):
-    # varint is a byte string in big endian.
-    # we need to drop the MSB from each byte and read the number as little endian
+def decode(varint):
+    # Drop the MSB from each byte and read the number as little endian
     out = 0
-    i = 0
-    while i < len(varint):
-        out |= (varint[i] & 0b01111111) << (7 * i)
-        i += 1
-    return out
-
-"""real    27m9.209s
-user    27m7.254s
-sys     0m1.670s
-"""
-def decode2(varint):
-    # varint is a byte string in big endian.
-    # we need to drop the MSB from each byte and read the number as little endian
-    out = 0
-    i = 0
-    while True:
-        try:
-            out |= (varint[i] & 0b01111111) << (7 * i)
-        except IndexError:
-            break
-        i += 1
+    for i,byte in enumerate(varint):
+        out |= (byte & 0b01111111) << (7 * i)
     return out
 
 def get_uint64(filename):
@@ -50,60 +28,73 @@ def get_uint64(filename):
         unsigned_int = f.read()
     return struct.unpack('>Q', unsigned_int)[0] # big endian
 
-# for i in range(1<<30):
-    # assert(decode(encode(i)) == i)
-def test1():
-    for i in range(1<<25):
-        assert(decode1(encode(i)) == i)
+test_data = range(1<<25)
+def test():
+    for i in test_data:
+        assert(decode(encode(i)) == i)
 
-def test2():
-    for i in range(1<<25):
-        assert(decode2(encode(i)) == i)
+start_time1 = time.perf_counter()
+test()
+end_time1 = time.perf_counter()
+print(f'{end_time1 - start_time1}') # 116.479612671
 
 def wrapper(func):
     def _wrapper():
         func()
     return _wrapper()
+cProfile.run('wrapper(test)')
 
-print('Decode1:')
-cProfile.run('wrapper(test1)')
-print('Decode2:')
-cProfile.run('wrapper(test2)')
+"""         cProfile output
+         199212934 function calls in 133.626 seconds
 
-# assert(decode(encode(get_uint64('maxint.uint64'))) == get_uint64('maxint.uint64'))
-# assert(decode(encode(get_uint64('150.uint64'))) == get_uint64('150.uint64'))
-# assert(decode(encode(get_uint64('1.uint64'))) == get_uint64('1.uint64'))
+   Ordered by: standard name
 
-# Decode1:
-#          9404166 function calls in 1.477 seconds
+   ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+        1    0.000    0.000  133.626  133.626 <string>:1(<module>)
+ 33554432   43.538    0.000   43.538    0.000 varint.py:19(decode)
+        1   15.172   15.172  133.626  133.626 varint.py:32(test)
+        1    0.000    0.000  133.626  133.626 varint.py:41(wrapper)
+        1    0.000    0.000  133.626  133.626 varint.py:42(_wrapper)
+ 33554432   64.811    0.000   74.917    0.000 varint.py:6(encode)
+        1    0.000    0.000  133.626  133.626 {built-in method builtins.exec}
+132104064   10.106    0.000   10.106    0.000 {method 'append' of 'list' objects}
+        1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
+"""
+
+
+# print('Decode1:')
+# print('Decode2:')
+# cProfile.run('wrapper(test2)')
+# # Decode1:
+#          364871430 function calls in 140.613 seconds
 
 #    Ordered by: standard name
 
 #    ncalls  tottime  percall  cumtime  percall filename:lineno(function)
-#         1    0.000    0.000    1.477    1.477 <string>:1(<module>)
-#   1048576    0.544    0.000    0.642    0.000 varint.py:21(decode1)
-#   1048576    0.562    0.000    0.642    0.000 varint.py:5(encode)
-#         1    0.193    0.193    1.477    1.477 varint.py:55(test1)
-#         1    0.000    0.000    1.477    1.477 varint.py:63(wrapper)
-#         1    0.000    0.000    1.477    1.477 varint.py:64(_wrapper)
-#         1    0.000    0.000    1.477    1.477 {built-in method builtins.exec}
-#   4177792    0.098    0.000    0.098    0.000 {built-in method builtins.len}
-#   3129216    0.080    0.000    0.080    0.000 {method 'append' of 'list' objects}
+#         1    0.000    0.000  140.613  140.613 <string>:1(<module>)
+#  33554432   52.357    0.000   63.413    0.000 varint.py:22(decode1)
+#         1   12.824   12.824  140.613  140.613 varint.py:54(test1)
+#  33554432   55.315    0.000   64.377    0.000 varint.py:6(encode)
+#         1    0.000    0.000  140.613  140.613 varint.py:62(wrapper)
+#         1    0.000    0.000  140.613  140.613 varint.py:63(_wrapper)
+#         1    0.000    0.000  140.613  140.613 {built-in method builtins.exec}
+# 165658496   11.056    0.000   11.056    0.000 {built-in method builtins.len}
+# 132104064    9.062    0.000    9.062    0.000 {method 'append' of 'list' objects}
 #         1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
 
 
 # Decode2:
-#          5226374 function calls in 1.354 seconds
+#          199212934 function calls in 120.128 seconds
 
 #    Ordered by: standard name
 
 #    ncalls  tottime  percall  cumtime  percall filename:lineno(function)
-#         1    0.000    0.000    1.354    1.354 <string>:1(<module>)
-#   1048576    0.517    0.000    0.517    0.000 varint.py:35(decode2)
-#   1048576    0.571    0.000    0.648    0.000 varint.py:5(encode)
-#         1    0.189    0.189    1.354    1.354 varint.py:59(test2)
-#         1    0.000    0.000    1.354    1.354 varint.py:63(wrapper)
-#         1    0.000    0.000    1.354    1.354 varint.py:64(_wrapper)
-#         1    0.000    0.000    1.354    1.354 {built-in method builtins.exec}
-#   3129216    0.077    0.000    0.077    0.000 {method 'append' of 'list' objects}
+#         1    0.000    0.000  120.128  120.128 <string>:1(<module>)
+#  33554432   44.327    0.000   44.327    0.000 varint.py:36(decode2)
+#         1   13.019   13.019  120.128  120.128 varint.py:58(test2)
+#  33554432   54.163    0.000   62.782    0.000 varint.py:6(encode)
+#         1    0.000    0.000  120.128  120.128 varint.py:62(wrapper)
+#         1    0.000    0.000  120.128  120.128 varint.py:63(_wrapper)
+#         1    0.000    0.000  120.128  120.128 {built-in method builtins.exec}
+# 132104064    8.619    0.000    8.619    0.000 {method 'append' of 'list' objects}
 #         1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
